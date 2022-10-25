cbb8b425bfb993ac5ffefcf5ba039f8b9d9158d4
lopo <lopo@zju.edu.cn>
2022-10-25 22:08:57 +0800

    deps: tokio reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
+use std::collections::HashMap;
+use std::error::Error;
 use rocket::{get, main, routes};
 
-#[get("/")]
-async fn hello() -> String {
-    "hello rust!".to_string()
-}
+// #[get("/")]
+// async fn index() -> String {
+//     "hello rust!".to_string()
+// }
+//
+// #[allow(unused)]
+// #[main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     rocket::build()
+//         .mount("/", routes![hello])
+//         .launch().await?;
+//
+//     Ok(())
+// }
 
-#[allow(unused)]
-#[main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    rocket::build()
-        .mount("/hello", routes![hello])
-        .launch().await?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = reqwest::Client::new();
+
+    let mut body = HashMap::new();
+    body.insert("receive_id", "oc_51c8c6584bf4c1a3b01723bdec14d2da");
+    body.insert("msg_type", "text");
+    body.insert("content", "{\"text\":\"111\"}");
+    body.insert("uuid", "1");
+
+    let res = client.post("https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id")
+        .json(&body)
+        .header("Authorization", "Bearer t-g104aplaOIZDEKG3VMILKIKZBMCDKSMOEDBGIPPF")
+        .header("Content-Length", "application/json")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("res: {:#?}", res);
 
     Ok(())
 }
